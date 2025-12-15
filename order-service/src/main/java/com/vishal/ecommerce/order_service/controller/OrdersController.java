004574aba2f12f59e984108d95248a4bf390ab4c
vishal patidar <136967663+vishal-patidar01@users.noreply.github.com>
2025-12-15 21:32:45 +0530

    Add create order API endpoint

--- order-service/src/main/java/com/vishal/ecommerce/order_service/controller/OrdersController.java
+++ order-service/src/main/java/com/vishal/ecommerce/order_service/controller/OrdersController.java
@@ -1,24 +1,36 @@
 package com.vishal.ecommerce.order_service.controller;
 
+import com.vishal.ecommerce.order_service.clients.InventoryOpenFeignClient;
 import com.vishal.ecommerce.order_service.dto.OrderRequestDto;
 import com.vishal.ecommerce.order_service.service.OrdersService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @Slf4j
 @RequiredArgsConstructor
-@RequestMapping("/orders")
+@RequestMapping("/core")
 public class OrdersController {
 
     private final OrdersService ordersService;
+
+
+
+    @GetMapping("/helloOrders")
+    public String helloOrders() {
+        return "Hello from Orders Service";
+    }
+
+
+    @PostMapping("/create-order")
+    public ResponseEntity<OrderRequestDto> createOrder(@RequestBody OrderRequestDto orderRequestDto) {
+        OrderRequestDto orderRequestDto1 = ordersService.createOrder(orderRequestDto);
+        return ResponseEntity.ok(orderRequestDto1);
+    }
 
     @GetMapping
     public ResponseEntity<List<OrderRequestDto>> getAllOrders() {
