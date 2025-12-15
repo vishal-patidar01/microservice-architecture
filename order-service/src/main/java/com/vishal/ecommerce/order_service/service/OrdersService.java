05a4e725e35f198fa01e2a20b1d4843f37a9de17
vishal patidar <136967663+vishal-patidar01@users.noreply.github.com>
2025-12-15 21:36:13 +0530

    Add Retry, RateLimiter and CircuitBreaker

--- order-service/src/main/java/com/vishal/ecommerce/order_service/service/OrdersService.java
+++ order-service/src/main/java/com/vishal/ecommerce/order_service/service/OrdersService.java
@@ -1,8 +1,15 @@
 package com.vishal.ecommerce.order_service.service;
 
+import com.vishal.ecommerce.order_service.clients.InventoryOpenFeignClient;
 import com.vishal.ecommerce.order_service.dto.OrderRequestDto;
+import com.vishal.ecommerce.order_service.entity.OrderItem;
+import com.vishal.ecommerce.order_service.entity.OrderStatus;
 import com.vishal.ecommerce.order_service.entity.Orders;
 import com.vishal.ecommerce.order_service.repository.OrdersRepository;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import io.github.resilience4j.retry.annotation.Retry;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
@@ -17,6 +24,7 @@ public class OrdersService {
 
     private final OrdersRepository ordersRepository;
     private final ModelMapper modelMapper;
+    private final InventoryOpenFeignClient inventoryOpenFeignClient;
 
     public List<OrderRequestDto> getAllOrders() {
         log.info("Fetching all orders");
@@ -28,5 +36,33 @@ public class OrdersService {
         log.info("Fetching order with ID: {}", id);
         Orders order = ordersRepository.findById(id).orElseThrow(() -> new RuntimeException("Order not found"));
         return modelMapper.map(order, OrderRequestDto.class);
+    }
+
+
+
+
+//    @Retry(name = "inventoryRetry", fallbackMethod = "createOrderFallback")
+//    @RateLimiter(name = "inventoryRateLimiter", fallbackMethod = "createOrderFallback")
+    @CircuitBreaker(name = "inventoryCircuitBreaker", fallbackMethod = "createOrderFallback")
+    public OrderRequestDto createOrder(OrderRequestDto orderRequestDto) {
+        log.info("Calling the createOrder method");
+        Double totalPrice = inventoryOpenFeignClient.reduceStocks(orderRequestDto);
+
+        Orders orders = modelMapper.map(orderRequestDto, Orders.class);
+        for(OrderItem orderItem: orders.getItems()) {
+            orderItem.setOrder(orders);
+        }
+        orders.setTotalPrice(totalPrice);
+        orders.setOrderStatus(OrderStatus.CONFIRMED);
+
+        Orders savedOrder = ordersRepository.save(orders);
+
+        return modelMapper.map(savedOrder, OrderRequestDto.class);
+    }
+
+    public OrderRequestDto createOrderFallback(OrderRequestDto orderRequestDto, Throwable throwable) {
+        log.error("Fallback occurred due to: {}", throwable.getMessage());
+
+        return new OrderRequestDto();
     }
 }
