7507e34bc798deb68d0e6f548d443a817778699a
vishal patidar <136967663+vishal-patidar01@users.noreply.github.com>
2025-12-15 21:40:21 +0530

    Configure RestClient bean

--- inventory-service/src/main/java/com/vishal/ecommerce/inventory_service/config/AppConfig.java
+++ inventory-service/src/main/java/com/vishal/ecommerce/inventory_service/config/AppConfig.java
@@ -3,6 +3,7 @@ package com.vishal.ecommerce.inventory_service.config;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestClient;
 
 @Configuration
 public class AppConfig {
@@ -10,5 +11,10 @@ public class AppConfig {
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
+    }
+
+    @Bean
+    public RestClient restClient() {
+        return RestClient.builder().build();
     }
 }
