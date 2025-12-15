ccff8890acd30a49b11aedc359219acb182b351b
vishal patidar <136967663+vishal-patidar01@users.noreply.github.com>
2025-12-15 21:20:33 +0530

    Add OrderRequestItem Dto for inventory service

---
+++ inventory-service/src/main/java/com/vishal/ecommerce/inventory_service/dto/OrderRequestItemDto.java
@@ -0,0 +1,9 @@
+package com.vishal.ecommerce.inventory_service.dto;
+
+import lombok.Data;
+
+@Data
+public class OrderRequestItemDto {
+    private Long productId;
+    private Integer quantity;
+}
